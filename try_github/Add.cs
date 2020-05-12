a3cb2b1aa84d3b672810b21233e3384d5e6db1ac
akaminet133 <akaminet@occ.co.jp>
2020-05-12 17:45:49 +0900

    ごり押しプログラムに変更

--- try_github/Add.cs
+++ try_github/Add.cs
@@ -6,7 +6,7 @@ namespace try_github
 {
     class Add
     {
-        public int add(int a, int b)
+        public static int add(int a, int b)
         {
             return a + b;
         }
--- try_github/Program.cs
+++ try_github/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
 
 namespace try_github
 {
@@ -6,8 +9,17 @@ namespace try_github
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello GitHub!");
+
+            Console.WriteLine("2桁の数値を入力し、足し算と引き算を行うプログラム");
+            Console.Write("値を入力してください\na：");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("　b:");
+            int b = int.Parse(Console.ReadLine());
+            int total, total2;
+            total = Add.add(a, b);
+            total2 = Sub.sub(a, b);
+            Console.WriteLine("計算結果\n足し算：" + total +"\n引き算："+ total2 );
+
         }
     }
 }
